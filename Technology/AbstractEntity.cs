99709591fa503eeb747493d1fb2a158f156cecd5
Donna Seidel <dseidel01@gmail.com>
2021-08-31 21:58:05 -0500

    change nextId to private static int and lowercase

--- Technology/AbstractEntity.cs
+++ Technology/AbstractEntity.cs
@@ -7,12 +7,12 @@ namespace Technology
     public class AbstractEntity
     {
         public int DeviceId { get; }
-        public static int NextId = 1; //set a field not a property
+        private static int nextId = 1; //set a field not a property
 
         public AbstractEntity() //empty constructor
         {
-            DeviceId = NextId; //defaulted as 1 - set the property to the field value
-            NextId++; //increment NextId for the next entity that is created
+            DeviceId = nextId; //defaulted as 1 - set the property to the field value
+            nextId++; //increment NextId for the next entity that is created
         }
     }
 }
